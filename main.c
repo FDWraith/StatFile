a0768b227038ac3a1ad5e4a4bdaba00fdeeb8724
Kevin Zhang <kevinzhang3702@gmail.com>
2016-10-31 11:31:07 -0400

    did the first task, will work on second one now.

--- main.c
+++ main.c
@@ -14,7 +14,40 @@ int main(){
   if( err < 0){
     printf("Error message:[%s]\n", strerror(errno));
   }
-  printf("File Size: [%d]\n", buffer.st_size);
+
+  int counter = 0;
+  long s = buffer.st_size;
+  while( s > 1000 ){
+    counter ++;
+    s = s / 1000;
+  }
+
+  //Not most efficient, but I'll use if statements unless osmething better comes to mind
+  char end[2];
+  switch(counter) {
+  case 0 :
+    printf("Triggered Case 0\n");
+    strcpy(end, "B");
+    break;
+  case 1 :
+    printf("Triggered Case 1\n");
+    strcpy(end, "KB");
+    break;
+  case 2 :
+    printf("Triggered Case 2\n");
+    strcpy(end, "MB");
+    break;
+  case 3 :
+    printf("Triggered Case 3\n");
+    strcpy(end, "GB");
+    break;
+  case 4:
+    printf("Triggered Case 4\n");
+    strcpy(end, "TB");
+    break;
+  }
+  
+  printf("File Size: [%d%s]\n", s, end );
   printf("Mode: [%o]\n", buffer.st_mode);
   printf("Time of Last Access: [%s]\n", ctime(&buffer.st_atim));
   return 0;
